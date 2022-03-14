2645c2a0b051d74a318f49841011ceeb6aa859c0
Ahsan Zahoor <ahsanzahoor010@gmail.com>
2022-03-14 14:56:55 +0500

    clean up layout

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Get rid of error settings(field_with_errors) that rails is setting in input field by default.
+ActionView::Base.field_error_proc=Proc.new do |html_tag,instance|
+  html_tag.html_safe
+end
